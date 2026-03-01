fbd6c9ef7e5ff1923aa1e31a0adf89d7aeb910ca
Nicolas Schejtman <nschejtman93@gmail.com>
2026-02-28 19:15:31 -0800

    Initial implementation reading file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import torch
+
 def main():
-    print("Hello from llm!")
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+        print("Total number of characters:", len(raw_text))
+        print(raw_text[:99])
 
 
 if __name__ == "__main__":
